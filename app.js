4c5424ee3d242194e08e1eff9c40d2da3b79d251
Bekzod2255 <78213759+Bekzod2255@users.noreply.github.com>
2021-02-20 10:52:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", function() {
+    var navScroll = document.getElementById('vlol');
+    navScroll.classList.toggle("fixed", window.scrollY > 10);
+})
+
+window.addEventListener("")
